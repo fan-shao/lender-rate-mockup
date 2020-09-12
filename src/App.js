5e7e1050abe9202d2574af8da26740be7d3fbb70
Fan <fanny.shao18@gmail.com>
2020-09-11 21:27:34 -0400

    basic skeleton in top component

--- src/App.js
+++ src/App.js
@@ -1,58 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+        <label for="loan_size">Loan Size</label> 
+        <input type="text" name="loan_size" id="loan_size" />
+
+        <label for="credit_score">Credit Score</label> 
+        <input type="text" name="credit_score" id="credit_score" />
+
+        <label for="prop_type">Property Type </label>
+        <select name="prop_type" id="prop_type">
+          <option value="single_family">Single Family</option>
+          <option value="condo">Condo</option>
+          <option value="townhouse">Townhouse</option>
+          <option value="multi_family">Multi-Family</option>
+        </select>
+
+        <label for="occupancy">Occupancy </label>
+        <select name="occupancy" id="occupancy">
+          <option value="primary">Primary</option>
+          <option value="secondary">Secondary</option>
+          <option value="investment">Investment</option>
+        </select>
+
+        <button id="quote_rates">Quote Rates</button>
+
+
+       <table>
+         <tr>
+           <th>Lender</th>
+           <th>Product</th>
+           <th>Rate</th>
+           <th>Closing Costs</th>
+           <th>Monthly Payment</th>
+         </tr>
+         {/* where api calls are going to go */}
+       </table>
     </div>
   );
 }
+
+
 
 export default App;
